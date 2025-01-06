d542020f13cb578879de2567e9b38128104f50ff
MaxBlut <maxime.bluteau@isen-ouest.yncrea.fr>
2025-01-06 17:17:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ wlMin = 402
 R = round((700-wlMin)/2) 
 G = round((546.1-wlMin)/2)
 B = round((435.8-wlMin)/2)
-print("hello world")
+
 
 def main():
     img = sp.open_image("feuille_250624_ref.hdr")
