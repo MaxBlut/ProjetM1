0ab2b05d21c31384b503db6ea57271ac3b634b1c
MaxBlut <maxime.bluteau@isen-ouest.yncrea.fr>
2025-01-09 11:11:45 +0100

    get_rgb main.py
    utilisation d'une nouvelle fonction qui trouve automatiquement les bonnes valeurs de RGB pour afficher l'image. 
    l'image généré est plus fidele à l'original mais toujours pas parfaite

--- main.py
+++ main.py
@@ -3,26 +3,11 @@ import spectral as sp
 
 sp.settings.envi_support_nonlowercase_params = True
 
-wlMin = 402
-R = round((700-wlMin)/2) 
-G = round((546.1-wlMin)/2)
-B = round((435.8-wlMin)/2)
-print("hello world")
-#gros bebe
 
 def main():
     img = sp.open_image("feuille_250624_ref.hdr")
-    print(img)
-    
-    data = img.load()
-    print(data)
-
-    
-   
-    view = sp.imshow(img,(R,G,B))
+    sp.imshow(sp.get_rgb(source = img))
     input("Press Enter to close the program...")
-
-    print(view)
     return 0
 
 
