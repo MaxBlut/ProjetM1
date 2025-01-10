cc89e4e74ce10e057176112006e51684cd30cfdd
MaxBlut <maxime.bluteau@isen-ouest.yncrea.fr>
2025-01-10 09:44:27 +0100

    changement du nom des fichiers
    j'ai changer le noms des fichier pour les renre plus comprehensible et pour qu'ils se generent automatiquement grace a un nom de fichier en input

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ np.random.seed(42)
 
 
 
-def first_kmean(filemame = "feuille_250624_ref.hdr"):
+
+def change_filename(filename, txt):
+    newfilename = list(filename)            #we change the name of the file to save the cluster map
+    for i in range(4):                      #we create the name of the new file from the input file name
+        newfilename.pop()
+    newfilename += txt
+    newfilename = ''.join(newfilename)
+    return newfilename
+
+
+def first_kmean(img_filename = "feuille_250624_ref.hdr"):
     # the first kmean is used to cluster the image into 2 clusters (background and leaf)
-    data = sp.open_image(filemame).load()
+    data = sp.open_image(img_filename).load()
     m, _ = sp.kmeans(data, 2, 20)
-    np.save("cluster_map.npy", m)
+    np.save(change_filename(img_filename, "_background_map.npy"), m)
     return 0 
 
 
-def extract_leaf_cluster(img_filename = "feuille_250624_ref.hdr", cluster_map_filename = "cluster_map.npy"):
-    #this function is used to extract the leaf cluster from the image
+def extract_one_cluster(img_filename = "feuille_250624_ref.hdr", cluster_map_filename = "feuille_250624_ref_background_map.npy"):
+    #this function is used to extract one cluster from the image
     #it takes the image and the cluster map as input and return a new hdr file with only the pixels of the leaf
     img = sp.open_image(img_filename)
     data = img.load()
@@ -35,7 +45,7 @@ def extract_leaf_cluster(img_filename = "feuille_250624_ref.hdr", cluster_map_fi
         'wavelength': img.metadata.get('wavelength', []),
     }
     sp.envi.save_image(
-        'clustered_cluster.hdr',
+        change_filename(img_filename, "_selected_cluster.hdr"),
         subset_data.reshape(1, -1, subset_data.shape[1]),  # Reshape en 1 ligne
         metadata=new_header,
         force=True
@@ -43,36 +53,36 @@ def extract_leaf_cluster(img_filename = "feuille_250624_ref.hdr", cluster_map_fi
     return 0
 
 
-def second_kmean(n_clusters = 3, cluster_filename = "clustered_cluster.hdr"):
+def second_kmean(n_clusters = 3, iteration = 20, cluster_filename = "feuille_250624_ref_selected_cluster.hdr"):
     #this function is used to cluster the leaf into n clusters to recognize the different parts of the leaf
     data = sp.open_image(cluster_filename).load()
-    m, _ = sp.kmeans(data, n_clusters, 20)
-    np.save("cluster_map_leaf.npy", m)
+    m, _ = sp.kmeans(data, n_clusters, iteration)
+    np.save(change_filename(cluster_filename, "_reclustered.npy"), m)
     return 0
 
 
-def color_cluster(leaf_clusters_filename = "cluster_map_leaf.npy",cluster_map_filename = "cluster_map.npy"):
+def color_cluster(second_cluster_map = "feuille_250624_ref_selected_cluster_reclustered.npy",fisrt_clusters_map = "feuille_250624_ref_background_map.npy"):
     #this function is used to color the different clusters of the leaf 
     #the background is colored in black and the different clusters of the leaf are colored in different colors
-    fisrt_clusters_map = np.load(cluster_map_filename)
-    second_cluster_map = np.load(leaf_clusters_filename)
+    fisrt_clusters_map = np.load(fisrt_clusters_map)
+    second_cluster_map = np.load(second_cluster_map)
     classes = np.full((fisrt_clusters_map.shape[0], fisrt_clusters_map.shape[1]), 0, dtype=int) #we initialize the classes with 0 (black) for the background
 
     k=0   #we use a variable k because the dimentions of the second cluster map are 1xN with N the number of pixels that has been considered to be appart of the leaf
     for i in range(fisrt_clusters_map.shape[0]):
         for j in range(fisrt_clusters_map.shape[1]):
-            if fisrt_clusters_map[i,j] == 1:
-                classes[i,j] = second_cluster_map[0][k]+1   #the for each pixel of the leaf we color it with the color of the cluster it belongs to 
-                k+=1                                        #we add 1 to the color because indices of the second cluster map also start from 0 but they are nt the background
+            if fisrt_clusters_map[i,j] == 1:                #if the pixel is part of the leaf
+                classes[i,j] = second_cluster_map[0][k]+1   #for each pixel of the leaf we color it with the color of the cluster it belongs to 
+                k+=1                                        #we add 1 to the color because indices of the second cluster map also start from 0 but they are not the background
     sp.imshow(classes=classes)                              #we display the colored image
     input("Press Enter to close the program...")
     return 0
 
 
 def main():
-    first_kmean()
-    extract_leaf_cluster()
-    second_kmean(3)
+    # first_kmean()
+    # extract_leaf_cluster()
+    # second_kmean(10,18)
     color_cluster()
     return 0
 
