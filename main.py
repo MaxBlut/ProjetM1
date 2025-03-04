fe7af83a7c70634beef24c0de92e370e57d45fad
MaxBlut <maxime.bluteau@isen-ouest.yncrea.fr>
2025-03-04 09:21:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,56 @@
 import spectral as sp
-
+import matplotlib.pyplot as plt
 
 sp.settings.envi_support_nonlowercase_params = True
 
 wlMin = 402
-# 650 550 450 
-# R = round((700-wlMin)/2) 
-# G = round((546.1-wlMin)/2)
-# B = round((435.8-wlMin)/2)
-R = round((639-wlMin)/2) 
-G = round((549-wlMin)/2)
-B = round((459-wlMin)/2)
-print("hello world")
+R = round((700-wlMin)/2) 
+G = round((546.1-wlMin)/2)
+B = round((435.8-wlMin)/2)
+
+
+
+
+
+def plot_poly(coords):
+    plt.plot((int(coords[0][0]),int(coords[0][1])), (int(coords[1][0]),int(coords[1][1])), 'ro-')
+    # for i in range(4):
+    #     plt.plot((int(coords[i][0]),int(coords[i][1])), (int(coords[(i+1)%4][0]),int(coords[(i+1)%4][1])), 'ro-')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
     img = sp.open_image("feuille_250624_ref.hdr")
-    print(img)
-    
     data = img.load()
-    print(data)
-
-    
    
-    view = sp.imshow(img,(R,G,B))
-    input("Press Enter to close the program...")
+    view = sp.imshow(img, (R,G,B))
+    input_var = ""
+    coords = []
+    while input_var !="y":
+        coords = plt.ginput(4)
+        if(len(coords) != 4):
+            print("Il faut 4 points !")
+            input_var = ""
+        else:
+            print(coords)
+            plot_poly(coords)
+            input_var = input("Etes vous satisfait de l'image ? (y/n) :\n")
 
-    print(view)
+
+   
+
+    plt.close()
     return 0
 
 
