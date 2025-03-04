7dca62435e909266f97dcd161ba1f0a52d929d86
MaxBlut <maxime.bluteau@isen-ouest.yncrea.fr>
2025-03-04 11:34:26 +0100

    intersecting

---
+++ intersecting.py
@@ -0,0 +1,37 @@
+NO = 0
+YES = 1
+COLLINEAR = 2
+
+def are_intersecting(v1x2, v1y2, v2x1, v2y1, v2x2, v2y2, v1x1=0, v1y1=0):
+    # Convert vector 1 to a line (line 1) of infinite length.
+    a1 = v1y2 - v1y1
+    b1 = v1x1 - v1x2
+    c1 = (v1x2 * v1y1) - (v1x1 * v1y2)
+
+    # Insert (x1,y1) and (x2,y2) of vector 2 into the equation above.
+    d1 = (a1 * v2x1) + (b1 * v2y1) + c1
+    d2 = (a1 * v2x2) + (b1 * v2y2) + c1
+
+    # If d1 and d2 both have the same sign, they are both on the same side
+    if (d1 > 0 and d2 > 0) or (d1 < 0 and d2 < 0):
+        return NO
+
+    # Calculate the infinite line 2 in linear equation standard form.
+    a2 = v2y2 - v2y1
+    b2 = v2x1 - v2x2
+    c2 = (v2x2 * v2y1) - (v2x1 * v2y2)
+
+    # Calculate d1 and d2 again, this time using points of vector 1.
+    d1 = (a2 * v1x1) + (b2 * v1y1) + c2
+    d2 = (a2 * v1x2) + (b2 * v1y2) + c2
+
+    # Again, if both have the same sign (and neither one is 0),
+    if (d1 > 0 and d2 > 0) or (d1 < 0 and d2 < 0):
+        return NO
+
+    # If they are collinear, they intersect in any number of points from zero to infinite.
+    if (a1 * b2) - (a2 * b1) == 0.0:
+        return YES
+
+    # If they are not collinear, they must intersect in exactly one point.
+    return YES
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import numpy as np
 import spectral as sp
 import matplotlib.pyplot as plt
+import intersecting
 
 sp.settings.envi_support_nonlowercase_params = True
 
@@ -8,51 +10,67 @@ R = round((700-wlMin)/2)
 G = round((546.1-wlMin)/2)
 B = round((435.8-wlMin)/2)
 
+img = sp.open_image("feuille_250624_ref.hdr")
+data = img.load()
+
+
+
+def plot_poly(X, Y):
+    print(X)
+    print(Y)    
+    n =  len(X)
+    print(n)
+    for i in range(n):
+        plt.plot([X[i],X[(i+1)%n]], [Y[i],Y[(i+1)%n]], 'ro-')
+    return 0
 
 
 
 
-def plot_poly(coords):
-    plt.plot((int(coords[0][0]),int(coords[0][1])), (int(coords[1][0]),int(coords[1][1])), 'ro-')
-    # for i in range(4):
-    #     plt.plot((int(coords[i][0]),int(coords[i][1])), (int(coords[(i+1)%4][0]),int(coords[(i+1)%4][1])), 'ro-')
 
-
-
-
-
-
-
-
-
+def check_inside_poly(X,Y):
+    n = len(X)
+    shape = img.shape
+    map = np.full((shape[0], shape[1]), 0, dtype=bool)
+    parite = 0
+    print(min(X), max(X))
+    print(min(Y), max(Y))
+    for i in range(min(X), max(X)):
+        for j in range(min(Y), max(Y)):
+            parite = 0
+            for k in range(n):
+                parite += intersecting.are_intersecting(j,i,X[k],Y[k],X[(k+1)%n],Y[(k+1)%n]) == 1
+                if(parite % 2 == 0) and (parite != 0):
+                    map[i,j] = True
+    view = sp.imshow(data = data , classes=map)
+    view.class_alpha=0.2
+    view.set_display_mode("classes")
+    view.refresh()
+    plt.ginput(1)
+    plt.close()
+    return 0
 
 
 
 
 
 def main():
-    img = sp.open_image("feuille_250624_ref.hdr")
-    data = img.load()
-   
-    view = sp.imshow(img, (R,G,B))
     input_var = ""
-    coords = []
     while input_var !="y":
-        coords = plt.ginput(4)
-        if(len(coords) != 4):
-            print("Il faut 4 points !")
+        view = sp.imshow(img, (R,G,B),title="tracez un polygone en indiquant les angles avec clique gauche, vous pouvez annuler avec clique droit et mettre fin avec le clic molette")
+        coords = plt.ginput(50)
+        if(len(coords) < 3):
+            print("Il faut au moin 3 points !")
             input_var = ""
         else:
-            print(coords)
-            plot_poly(coords)
+            X = [int(value[0]) for value in coords]
+            Y = [int(value[1]) for value in coords] 
+            print(data.shape)
+            plot_poly(X,Y)
             input_var = input("Etes vous satisfait de l'image ? (y/n) :\n")
-
-
-   
-
-    plt.close()
+        plt.close()
+    check_inside_poly(X,Y)
     return 0
 
 
 print(main())
-
